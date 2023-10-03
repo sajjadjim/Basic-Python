1abebf406d9ec712a148a0bb0795e1f98f2edb5d
S.M. SAJJAD HOSAIN JIM <120739140+sajjadjim@users.noreply.github.com>
2023-10-04 00:52:48 +0600

    Add files via upload

---
+++ !.py
@@ -0,0 +1,8 @@
+a = 5
+b = 6
+ 
+def sum(a,b):
+  return a+b
+ 
+ # function call
+print(sum(a,b))
---
+++ elements of matrix.py
@@ -0,0 +1,15 @@
+n = 3
+m = 3
+arr = [[3, 2, 7], [2, 6, 8], [5, 1, 9]]
+sum = 0
+
+# Iterating over all 1-D arrays in 2-D array
+for i in range(n):
+	# Printing all elements in ith 1-D array
+	for j in range(m):
+		# Printing jth element of ith row
+		sum += arr[i][j]
+
+print(sum)
+
+# This code id contributed by shivhack999
