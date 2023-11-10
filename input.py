407643f985f533d3526870be0fcae61ce617ae16
S.M. SAJJAD HOSAIN JIM <120739140+sajjadjim@users.noreply.github.com>
2023-11-10 19:40:03 +0600

    Update input.py

--- input.py
+++ input.py
@@ -2,7 +2,7 @@ name = input("Enter your name =")
 age = input("Enter your age =")
 
 
-print("~~~~~ Student Information ~~~~~")
+print("~~~~~ Student  Information ~~~~~")
 print("Name is ",name)
 print("Age ",age)
 
