6dd5075507819c84e10a7c53165fbcf28c4564e4
S.M. SAJJAD HOSAIN JIM <120739140+sajjadjim@users.noreply.github.com>
2023-11-26 00:26:12 +0600

    Add files via upload

---
+++ Typecasting.py
@@ -0,0 +1,11 @@
+num1 = int(input("Enter first number :"))
+num2 = int(input("Enter second number :"))
+
+result = (num1) + (num2) 
+print("The sum value is :",result)
+
+result = (num1) - (num2) 
+print("The sum value is :",result)
+
+result = (num1) - (num2) 
+print("The sum value is :",result)
