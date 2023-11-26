4c74f00ef0a6ab93311de78c43b3b3a804b608f3
S.M. SAJJAD HOSAIN JIM <120739140+sajjadjim@users.noreply.github.com>
2023-11-27 01:40:56 +0600

    Update First.py

--- First.py
+++ First.py
@@ -5,4 +5,4 @@ print("My first programming python course")
 print("print(My first programming python course)")
 
 My name is zim
-print("JIM "+" Helow world ......")
+print("JIM "+" Helow world ......") 
