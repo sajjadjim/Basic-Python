cfa9c54b5cee749ab33c4a9b9c9208b25e0276f9
SAJJAD HOSAIN JIM <120739140+sajjadjim@users.noreply.github.com>
2024-01-04 00:16:09 +0600

    Add files via upload

---
+++ Letter Grade Program.py
@@ -0,0 +1,17 @@
+marks= 61
+if marks>=80 and marks<=100:
+    print("A+")
+elif marks<=79 and marks>=70:
+    print("A")
+elif marks<=69 and marks>=60:
+    print("A-")
+elif marks <= 59 and marks >= 50:
+    print("B")
+elif marks <= 49 and marks >= 40:
+    print("C")
+elif marks <= 39 and marks >= 33:
+    print("D")
+elif marks <= 32 and marks >= 0:
+    print("Fail")
+else:
+    print("Invalied Number")
