5949c799eca9cf550536169b40d99a4376b80cf2
SAJJAD HOSAIN JIM <120739140+sajjadjim@users.noreply.github.com>
2024-01-04 00:16:20 +0600

    Add files via upload

---
+++ List (Part-1).py
@@ -0,0 +1,16 @@
+subjects =["C","C++","Java","Python","Android"]
+
+print(subjects[0])
+
+print(subjects[1])
+
+print(subjects)
+
+print(subjects[-1]) #Reverse Print
+
+print("Python" in subjects)  #check
+print("Jim"not in subjects)
+
+print(subjects + ["jim",5364]) #Add more otems in List
+
+print(subjects *3 ) #Show the value 3 times
