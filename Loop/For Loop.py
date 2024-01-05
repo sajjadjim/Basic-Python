2a5f1963a914a8342a533c98e1c42931a95e84c9
SAJJAD HOSAIN JIM <120739140+sajjadjim@users.noreply.github.com>
2024-01-05 23:25:45 +0600

    Add files via upload

---
+++ Loop/For Loop.py
@@ -0,0 +1,7 @@
+num =[11,22,33,44,55,66,77,88,99]
+sum =0
+for x in num:
+    print(x)
+    sum= sum +x
+
+print("Totall Number of value:",sum)
---
+++ Loop/nested loop.py
@@ -0,0 +1,13 @@
+
+for i in range(1,6):
+    for j in range(1,i+1):
+     print("*",end= " ")
+    print()
+
+for i in range(6,0,-1):
+    for j in range(1,i+1):
+     print("*",end= " ")
+    print()
+
+
+#print("i=",i,"\t j=",j)
