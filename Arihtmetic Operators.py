dae6653daa480d3fa45b74a8ebbb94e4b4a5c2f4
SAJJAD HOSAIN JIM <120739140+sajjadjim@users.noreply.github.com>
2024-01-28 20:46:51 +0600

    Add files via upload

---
+++ Arihtmetic Operators.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+sum = a + b
+division = a - b
+multiple = a * b
+print(sum)
+print(division)
+print(multiple)
---
+++ Print Division.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    division = a/b
+    print(int(division))
+    print(float(division))
