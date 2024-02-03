65590aff554133bbfa9cf1a620ee25ad75ad9aa1
SAJJAD HOSAIN JIM <120739140+sajjadjim@users.noreply.github.com>
2024-02-04 00:36:05 +0600

    Add files via upload

---
+++ Loop/Nasted Loop 1.py
@@ -0,0 +1,10 @@
+adj =["Mango" , "JackFruits" , "BlackBarry"]
+second=["Tree" ,"Onnion" ,"Runner"]
+
+for i in adj:
+    for j in second:
+        print(i,j)
+
+for x in [0, 1, 2]:
+  pass      # for loops cannot be empty, but if you for some reason have a for loop with no content, put in the pass statement to avoid getting an error.
+
