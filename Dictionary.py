1831a9b9677b712a1132d28d56be2d6fbb4cf7c7
SAJJAD HOSAIN JIM <120739140+sajjadjim@users.noreply.github.com>
2024-03-16 00:10:06 +0600

    Create Dictionary.py

---
+++ Dictionary.py
@@ -0,0 +1,37 @@
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict)
+
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict["brand"])
+
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964,
+  "year": 2020
+}
+print(thisdict)
+
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964,
+  "year": 2020
+}
+print(len(thisdict))
+
+
+thisdict = dict(name = "John", age = 36, country = "Norway")
+print(thisdict) 
+
