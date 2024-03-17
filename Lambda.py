516a54bec1bfc41de758703ad9012f084a605fc6
SAJJAD HOSAIN JIM <120739140+sajjadjim@users.noreply.github.com>
2024-03-18 01:39:40 +0600

    Create Lambda.py

---
+++ Lambda.py
@@ -0,0 +1,12 @@
+x = lambda a, b, c : a + b + c
+print(x(5, 6, 2))
+
+
+def myfunc(n):
+  return lambda a : a * n
+
+mydoubler = myfunc(2)
+mytripler = myfunc(3)
+
+print(mydoubler(11)) 
+print(mytripler(11))
