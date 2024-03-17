7bcb4a30898c9b3a5c8d68ef91355a383731e1fb
SAJJAD HOSAIN JIM <120739140+sajjadjim@users.noreply.github.com>
2024-03-18 01:26:51 +0600

    Create List to convert Dictionary.py

---
+++ Dictionary/List to convert Dictionary.py
@@ -0,0 +1,9 @@
+my_list=[("jim",101) ,("Rifat" ,102) , ("mahfuj",103)]
+
+my_dict =dict(my_list)
+print(my_dict)
+
+################ Another Default Program
+create_List =["Jim" , "Rifat" , "Mahfuj" , "Tahid"]
+convert_dict ={key : value for key , value in enumerate(create_List)}
+print(convert_dict)
