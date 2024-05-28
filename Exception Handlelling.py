39b0f1e13e1f1464b82e09e368dab8213f9fc596
SAJJAD HOSAIN JIM <120739140+sajjadjim@users.noreply.github.com>
2024-05-29 01:56:49 +0600

    Create Exception Handlelling.py

---
+++ Exception Handlelling.py
@@ -0,0 +1,12 @@
+from logging import NullHandler
+def divide(x,y):
+  try: # Always Execute
+    result=x/y # x=10, y=2
+    print(result)
+  except: # Alternate of try
+    print("Error in try block")
+  finally:
+    print("End of division")
+
+divide(10,2)
+divide(10,0)
