d734366623d10fde72dbd85eb9418df8d1caae63
SAJJAD HOSAIN JIM <120739140+sajjadjim@users.noreply.github.com>
2024-06-17 00:02:13 +0600

    Update First.py

--- First.py
+++ First.py
@@ -1,6 +1,6 @@
 print("Hellow World... !")    
       
-print("My first programming python course")
+print("My first programming python course")  
 
 print("print(My first programming python course)")
  
