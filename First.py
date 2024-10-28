d8760904323b1db2f06726cb1eca3e43de5e5770
SAJJAD HOSAIN JIM <120739140+sajjadjim@users.noreply.github.com>
2024-10-28 23:46:25 +0600

    Update First.py

--- First.py
+++ First.py
@@ -4,5 +4,5 @@ print("My first programming python course")
 
 print("print(My first programming python course)") 
  
-print("My name is zim")
+print("My name is Sajjad Hossain jim")
 print("JIM "+" Helow world ......") 
