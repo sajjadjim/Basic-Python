ea718fc0448ab61fb1572c0aa1f154679284a2a7
SAJJAD HOSAIN JIM <120739140+sajjadjim@users.noreply.github.com>
2024-11-11 01:55:41 +0600

    Update Formatted String.py

--- Formatted String.py
+++ Formatted String.py
@@ -4,6 +4,6 @@ print("sum is ",num1 + num2)
 
 print(f"{num1} + {num2} = {num1+num2}")
  
-print("Sajjad Hossain Jim..",end=" ")
+print("Sajjad Hossain Jim..",end=" ") 
 print("016****18**") 
 print("Helow worl!")
